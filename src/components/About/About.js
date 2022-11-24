24e9b5dcd773f551a4dd3bccab3efc91c2344593
wali55 <walisantunu@gmail.com>
2022-11-24 14:32:56 +0600

    about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>about</h2>
+        <div className="m-16">
+            <h1 className="text-4xl text-center my-14">About Our <span className="text-orange-400">Products</span></h1>
+            <p className="text-lg">We're currently seeing the best end-of-year discounts on our favorite laptops like the Dell XPS 13, MacBook Air and MacBook Pro  just to name a few. So whether you're on the hunt for the best laptop for college, business laptop or versatile 2-in-1, now is the best time to snag one for less. And if you're on a really tight budget, a refurbished laptop is another money-saving option to consider. With tons of early Black Friday laptop deals now available, it's not too early to save. From laptops under $500 to sub-$2,500 machines, here are the best laptop deals you can get today. </p>
         </div>
     );
 };
